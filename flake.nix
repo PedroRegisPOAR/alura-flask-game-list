c088efadb8077d326990cab42890863702e0f5ec
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-10-21 13:38:39 -0300

    Cleaning flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-    description = "Flake of game list project";
+  description = "Flake of game list project";
 
-    inputs = {
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
 
@@ -13,29 +13,14 @@
           system = "x86_64-linux";
           config = { allowUnfree = true; };
         };
-
-        config = {
-          projectDir = ./.;
-        };
-
       in
       {
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
-            gnumake
-            poetry
             python3
           ];
 
           shellHook = ''
-            export TMPDIR=/tmp
-
-            test -f .venv/bin/activate || make poetry.install
-            source .venv/bin/activate
-
-            if ! test -f .env; then
-              make config.env
-            fi
             echo "Entering the nix devShell in alura-flask-game-list"
           '';
         };
